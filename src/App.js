8750a9dd7910993011838245b23b78482fcd3e15
XolaniMgube <xnmgube@gmail.com>
2020-06-01 13:07:41 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import Todos from './components/Todos';
+
+import './App.css';
+
+class App extends Component {
+
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Dinner with wife',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Meeting with Boss',
+        completed: false
+      }
+    ]
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Todos todos={this.state.todos} />
+      </div>
+    );
+  }
+}
+
+export default App;
