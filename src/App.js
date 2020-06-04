046628ccfa43a5d5c31ad8940739ccfe8d6e3cc6
XolaniMgube <xnmgube@gmail.com>
2020-06-04 20:47:21 +0200

    jsx and trying to add states

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ class App extends Component {
 
   state = {
     loading: true,
-    person: null
+    person: null,
+    list: null,
+    answer: null
   }
 
   async componentDidMount() {
@@ -15,19 +17,74 @@ class App extends Component {
     const data = await response.json()
     this.setState({person: data.Countries[154], loading: false})
     console.log(data.Countries[154])
+    // console.log(data.Countries)
+
+    // let sum = 4 + 4;
+    // this.setState({answer: sum, loading: false})
+
+    for (let i = 0; i < data.Countries.length; i++) {
+        let covidCountries = `<option> ${data.Countries[i].Country} </option>`
+        console.log(covidCountries)
+        this.setState({list: covidCountries, loading: false})  
+    }
   }
+
+  // countryList() {
+  //   for (let i = 0; i < data.Countries.length; i++) {
+        
+  //   }
+  // }
 
   render() {
     return (
-      <div className="App">
+      <div className="sections">
        {this.state.loading || !this.state.person ? (
         <div>loading...</div>    
       ) : (
-        <div>
-          <div>{this.state.person.Country}</div>
-          <div>{this.state.person.NewConfirmed}</div>
-          <div>{this.state.person.TotalConfirmed}</div>
-          <div>{this.state.person.Date}</div>
+        <div className="conditionTrue">
+
+        <select className="menu">
+          <option>{this.state.person.Country}</option>
+          {/* <option>{this.state.sum}</option> */}
+          {this.state.covidCountris}
+        </select>
+
+        <header className="header">
+          <h1>{this.state.person.Country}</h1>
+          <p>{this.state.person.Date}</p>
+        </header>
+          
+
+        <section className="totalConfirmed">
+          <h3>TOTAL CONFIRMED</h3>
+          <div className="bigNumber one">{this.state.person.TotalConfirmed}</div>
+        </section>
+
+        <section className="totalRecovered">
+          <h3>TOTAL RECOVERED</h3>
+          <div className="bigNumber two"> {this.state.person.TotalRecovered}</div>
+        </section>
+
+        <section className="totalDeaths">
+          <h3>TOTAL DEATHS</h3>
+          <div className="bigNumber three"> {this.state.person.TotalDeaths}</div>
+        </section>
+          
+        <section className="newConfirmed">
+          <h5>NEW CONFIRMED</h5>
+          <div className="smallNumber one">{this.state.person.NewConfirmed}</div>
+        </section>
+
+        <section className="newRecovered">
+          <h5>NEW RECOVERED</h5>
+          <div className="smallNumber two">{this.state.person.NewRecovered}</div>
+        </section>
+
+        <section className="newDeaths">
+          <h5>NEW DEATHS</h5>
+          <div className="smallNumber three">{this.state.person.NewDeaths}</div>
+        </section>
+          
         </div>
       )}
       </div>
